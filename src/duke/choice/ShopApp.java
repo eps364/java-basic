70f1a9e3c53b8f2eaf59f6fd80f29f580846c535
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2021-03-04 23:01:54 -0300

    Add Switch case

--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -19,9 +19,21 @@ public class ShopApp {
         double tax = 0.2;
         double total = 0;
         
+        
         Customer c1= new Customer();
         c1.name = "Pinky";
-        System.out.println("Nome: " + c1.name);
+        c1.size = "S";
+        
+        
+//        if(measurement <= 3 ){
+//            c1.size =  "S";
+//        } else if (measurement <= 6){
+//            c1.size = "M";
+//        } else if (measurement <=9){
+//            c1.size = "L";
+//        } else {
+//            c1.size = "X";
+//        }
         
         Clothing item1 = new Clothing();
         Clothing item2 = new Clothing();
@@ -34,12 +46,38 @@ public class ShopApp {
         item2.price = 10.5;        
         item2.size = "S";
         
+        System.out.println("Nome: " + c1.name);
+        System.out.println("Size: " + c1.size);
+        
         System.out.println(item1.description +";"+ item1.price+";"+item1.size);
         System.out.println(item2.description +";"+ item2.price+";"+item2.size);
         
         total = ((item1.price * tax)+item1.price) + ((item2.price * tax)+item2.price);
-        
         System.out.println("Total: "+ total);
+        
+        int measurement = 10;
+        
+        switch(measurement){
+            case 1:
+            case 2:
+            case 3:
+                c1.size = "S";
+                break;
+            case 4:
+            case 5: 
+            case 6:
+                c1.size = "M";
+                break;
+            case 7:
+            case 8:
+            case 9:
+                c1.size = "L";
+                break;
+            default:
+                c1.size = "X";
+        }
+        
+        System.out.println("Size of measurement: " + c1.size);
     }
     
 }
