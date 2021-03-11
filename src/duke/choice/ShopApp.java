ecf8c7b9951fbdbf7c544a94d7b067b68e75cc23
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2021-03-11 01:48:29 -0300

    Override

--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -34,7 +34,7 @@ public class ShopApp {
         System.out.println("Size: " + c1.getSize());
         System.out.println("Size of measurement: " + c1.getSize()+"\n");
         for (Clothing item : c1.getItems()) {
-            System.out.println("Item: " + item.toString());
+            System.out.println("Item: " + item);
         }
 
         System.out.println("\nTotal: " + c1.getTotalClothongCost());
